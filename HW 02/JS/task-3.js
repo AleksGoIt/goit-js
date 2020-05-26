b7abb09444188ef2737fd03ea6d8994c5b90801e
AleksGoIt <ablin4ik86.com>
2020-05-26 03:37:40 +0300

    ed

--- HW 02/JS/task-3.js
+++ HW 02/JS/task-3.js
@@ -10,9 +10,8 @@ const findLongestWord = function (string) {
   return biggestWord;
 };
 
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
 
-console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); /
-
-console.log(findLongestWord("Google do a roll")); 
+console.log(findLongestWord("Google do a roll"));
 
 console.log(findLongestWord("May the force be with you"));
